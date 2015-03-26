fbb95dc06faeb2f44069a8491c909aa51cfbb17e
Seth Hamlin <helloimseth@gmail.com>
2015-03-26 15:02:50 -0400

    Refactors snake class

--- snake.js
+++ snake.js
@@ -5,75 +5,52 @@
 
   var Snake = Nokia.Snake = function(pos, board){
     this.dir = "N";
+
     this.pos = pos;
     this.segments = [pos];
-    this.board = board;
     this.alive = true;
+
+    this.board = board;
 
     this.score = 0;
     this.modifier = 1;
   };
 
+  Snake.DIRSANDDELTAS = {
+    'N': [-1, 0],
+    'E': [0, 1],
+    'S': [1, 0],
+    'W': [0, -1]
+    };
+
   Snake.prototype.move = function(){
     var newFirst = _.first(this.segments).slice();
 
-    switch (this.dir){
-      case "N":
-        newFirst[0] -= 1;
-        break;
-      case "E":
-        newFirst[1] += 1;
-        break;
-      case "S":
-        newFirst[0] += 1;
-        break;
-      default:
-        newFirst[1] -= 1;
-        break;
-    }
+    newFirst[0] += Snake.DIRSANDDELTAS[this.dir][0];
+    newFirst[1] += Snake.DIRSANDDELTAS[this.dir][1];
 
     this.checkIfAlive(newFirst);
 
     this.segments.unshift(newFirst);
 
-    if(!this.board.isAnApple(newFirst)){
+    if(!this.ateAnApple(newFirst)){
       this.segments.pop();
-    } else {
-      this.score += 1 * this.modifier;
-
-      this.updateModifierAndDifficulty();
-
-      this.board.removeApple(newFirst);
     }
-
-
   };
 
   Snake.prototype.checkIfAlive = function (pos) {
-    if(this.isASegment(pos)){
-      this.alive = false;
-    } else if (pos[0] === -1 || pos[1] === -1 ||
-               pos[1] === this.board.DIM || pos[0] === this.board.DIM) {
+    if(this.isASegment(pos) || this.isOutOfBounds(pos)) {
       this.alive = false;
     }
   };
 
   Snake.prototype.turn = function(dir){
+    var currentDelta = Snake.DIRSANDDELTAS[this.dir];
+    var newDelta = Snake.DIRSANDDELTAS[dir];
+    var summedDeltas = [currentDelta[0] + newDelta[0],
+                        currentDelta[1] + newDelta[1]];
 
-    switch (this.dir){
-      case "N":
-        this.dir = dir === "S" ? this.dir : dir;
-        break;
-      case "E":
-        this.dir = dir === "W" ? this.dir : dir;
-        break;
-      case "S":
-        this.dir = dir === "N" ? this.dir : dir;
-        break;
-      default:
-        this.dir = dir === "E" ? this.dir : dir;
-        break;
-    }
+    this.dir = _.isEqual(summedDeltas, [0,0]) ? this.dir : dir;
   };
 
   Snake.prototype.isASegment = function (coord){
@@ -97,9 +74,30 @@
   };
 
   Snake.prototype.updateModifierAndDifficulty = function () {
-    if (this.segments.length % 10 === 0 && this.score > 1) {
+    if (this.segments.length % 10 === 0) {
       this.modifier++;
       this.board.view.increaseDifficulty();
     }
+  };
+
+  Snake.prototype.isOutOfBounds = function (pos) {
+    return pos[0] === -1 ||
+           pos[1] === -1 ||
+           pos[0] === this.board.DIM ||
+           pos[1] === this.board.DIM;
+  };
+
+  Snake.prototype.ateAnApple = function (pos) {
+    if(this.board.isAnApple(pos)){
+      this.score += 1 * this.modifier;
+
+      this.updateModifierAndDifficulty();
+
+      this.board.removeApple(pos);
+
+      return true;
+    }
+
+      return false;
   };
 })();
